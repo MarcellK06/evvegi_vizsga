c36bc809ab3447c1cb7b787d1387a20c2686fb70
Kádár Marcell <mrcllkadar@gmail.com>
2025-01-16 12:19:19 +0000

    reordered

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Request from "./Components/PriceQuote/Request";
-import ActiveRequests from "./Components/PriceQuote/ActiveRequests";
+import Home from "./Pages/Home";
 //192.168.1.45
 const root = ReactDOM.createRoot(document.getElementById("root"));
-import Home from "./Pages/Home";
 
 root.render(
   <>
-    <ActiveRequests />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
