343a691923656d73e14a4bee2431542eb9d06b4f
Kádár Marcell <kdrmarcell@gmail.com>
2025-01-16 11:39:44 +0000

    A bit of visualization for the sake of development

--- src/Components/PriceQuote/ActiveRequests.jsx
+++ src/Components/PriceQuote/ActiveRequests.jsx
@@ -13,19 +13,20 @@ constructor(title, description, vin, answered) {
     }
 
     const LoadRequests = () => {
-
+        //TODO
     }
 
     const RequestEntry = (el) => {
-        return (<>
+        return (<div style={{backgroundColor: el.answered ? "green":"red"}}>
         <p>{el.title}</p>
         <p>{el.description}</p>
         <p>{el.vin}</p>
-        <p>{el.answered}</p>
-        </>)
+        <p>Válaszolva: {el.answered ? "Igen" : "Nem"}</p>
+        <input type="button" value="Válasz Mutatása" style={{display: el.answered ? "block" : "none"}} />
+        </div>)
     }
 
-    var templateList = [new RequestStruct("Ez egy cím.", "Ez egy leírás.", "XXXXXXXXXXXXXXXX", "false")];
+    var templateList = [new RequestStruct("Ez egy cím.", "Ez egy leírás.", "XXXXXXXXXXXXXXXX", false),new RequestStruct("Ez egy másik cím.", "Ez egy másik leírás.", "YYYYYYYYYYYYYYYYY", true)];
 
     return (<>
     {templateList.map((i) => RequestEntry(i))}
